dc71be09360bb24c3b5d21f1fed4e5fc73995847
bhvozda <31354651+bhvozda@users.noreply.github.com>
2019-11-17 14:35:25 +0100

    routes fix

--- src/main/java/team/project/upb/UpbApplication.java
+++ src/main/java/team/project/upb/UpbApplication.java
@@ -2,11 +2,15 @@ package team.project.upb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import team.project.upb.api.security.CustomPasswordEncoder;
 
+@Controller
 @SpringBootApplication
-public class UpbApplication {
+public class UpbApplication implements ErrorController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UpbApplication.class, args);
@@ -15,6 +19,18 @@ public class UpbApplication {
 	@Bean
 	public CustomPasswordEncoder bCryptPasswordEncoder() {
 		return new CustomPasswordEncoder();
+	}
+
+	private static final String PATH = "/error";
+
+	@RequestMapping(value = PATH)
+	public String error() {
+		return "forward:/index.html";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return PATH;
 	}
 
 }
